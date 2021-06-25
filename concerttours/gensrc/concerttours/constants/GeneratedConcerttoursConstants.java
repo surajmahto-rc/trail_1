3a39c8d824ebe4673bbb6315cd627cba6e63e162
surajmahto.rc <surajmahto.rc@gmail.com>
2021-06-25 20:40:41 +0530

    Create a Listener:java files not important

--- concerttours/gensrc/concerttours/constants/GeneratedConcerttoursConstants.java
+++ concerttours/gensrc/concerttours/constants/GeneratedConcerttoursConstants.java
@@ -1,7 +1,7 @@
 /*
  * ----------------------------------------------------------------
  * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
- * --- Generated at Jun 25, 2021, 8:18:26 PM                    ---
+ * --- Generated at Jun 25, 2021, 8:38:51 PM                    ---
  * ----------------------------------------------------------------
  */
 package concerttours.constants;
@@ -20,6 +20,7 @@ public class GeneratedConcerttoursConstants
 		public static final String CONCERT = "Concert".intern();
 		public static final String CONCERTTYPE = "ConcertType".intern();
 		public static final String MUSICTYPE = "MusicType".intern();
+		public static final String NEWS = "News".intern();
 	}
 	public static class Attributes
 	{
--- concerttours/gensrc/concerttours/jalo/Concert.java
+++ concerttours/gensrc/concerttours/jalo/Concert.java
@@ -1,7 +1,7 @@
 /*
  * ----------------------------------------------------------------
  * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
- * --- Generated at Jun 25, 2021, 8:18:26 PM                    ---
+ * --- Generated at Jun 25, 2021, 8:38:51 PM                    ---
  * ----------------------------------------------------------------
  */
 package concerttours.jalo;
